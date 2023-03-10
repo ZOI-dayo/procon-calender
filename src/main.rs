dfc7ff1a0484b5f2051ca3036fa0166ba9bff183
ZOI_dayo <zoizoi.dayo@gmail.com>
2023-03-10 22:29:11 +0900

    get google api token

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,14 @@ use hyper_tls::HttpsConnector;
 
 use serde::{Deserialize, Serialize};
 
-use  flate2::bufread::GzDecoder;
+use flate2::bufread::GzDecoder;
 
 use chrono::{Utc, DateTime, Duration, TimeZone};
+
+use serde_json::json;
+
+use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
+
 
 #[tokio::main]
 async fn main() {
@@ -26,7 +31,9 @@ async fn main() {
         // problems
         get_problems(&client).await;
         get_moja(&client).await;
+        println!("{}", google_login(&client).await);
 
+        // TODO: 1~2h?
         sleep(std::time::Duration::from_secs(60));
     }
 }
@@ -147,6 +154,78 @@ async fn get_moja(client: &Client<HttpsConnector<hyper::client::HttpConnector>>)
 
 
     Ok(())
+}
+
+async fn google_login(client: &Client<HttpsConnector<hyper::client::HttpConnector>>) -> String {
+    #[derive(Debug, Deserialize)]
+    struct GoogleCredential {
+        r#type: String,
+        project_id: String,
+        private_key_id: String,
+        private_key: String,
+        client_email: String,
+        client_id: String,
+        auth_uri: String,
+        token_uri: String,
+        auth_provider_x509_cert_url: String,
+        client_x509_cert_url: String,
+    }
+    let google_credential: GoogleCredential = serde_json::from_reader(std::fs::File::open("secret/google_credential.json").unwrap()).unwrap();
+
+    let now = Utc::now();
+    let iat = now.timestamp();
+    let exp = (now + Duration::minutes(60)).timestamp();
+
+    #[derive(Debug, Serialize)]
+    struct Claims {
+        iss: String,
+        scope: String,
+        aud: String,
+        exp: i64,
+        iat: i64,
+    }
+    let mut header = Header::default();
+    header.typ = Some("JWT".to_string());
+    header.alg = Algorithm::RS256;
+
+    let my_claims =
+        Claims {
+            iss: google_credential.client_email,
+            scope: "https://www.googleapis.com/auth/spreadsheets".to_string(),
+            aud: google_credential.token_uri,
+            exp,
+            iat,
+        };
+
+    let jwt = encode(&header, &my_claims, &EncodingKey::from_rsa_pem(google_credential.private_key.as_bytes()).unwrap()).unwrap();
+
+    let token_body = json!({
+        "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        "assertion": jwt
+    });
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(my_claims.aud)
+        .header("Content-Type", "application/json")
+        .body(Body::from(token_body.to_string())).unwrap();
+    let mut resp = client.request(req).await.unwrap();
+
+    let mut result = String::new();
+    while let Some(chunk) = resp.body_mut().data().await {
+        result += &String::from_utf8((&chunk.unwrap()).to_vec()).unwrap();
+    }
+
+    #[derive(Debug, Deserialize)]
+    struct Token {
+        access_token: String,
+    }
+
+    let token_response_body: Token = serde_json::from_str(&result).unwrap();
+
+    // let access_token = token_response_body.get("access_token").unwrap().as_str();
+
+    return String::from(token_response_body.access_token);
 }
 
 // Debug
