26e6ac4a77ff58dee5a80d3675fbdc3ede52d138
ZOI-dayo <64856587+ZOI-dayo@users.noreply.github.com>
2023-05-06 23:16:04 +0900

    remove screen for actions

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 cd `dirname $0`
 cargo build --release
-screen -d -S procon-calender -m ./target/release/procon-calender
+# screen -d -S procon-calender -m ./target/release/procon-calender
+./target/release/procon-calender
